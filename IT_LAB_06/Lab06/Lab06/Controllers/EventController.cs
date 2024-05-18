9c1f5b8e8eaff08163be26118e897ab934ab880f
pupinoskii <Ivan Pupinoski>
2024-05-19 01:02:40 +0200

    LABS_IT

---
+++ IT_LAB_06/Lab06/Lab06/Controllers/EventController.cs
@@ -0,0 +1,105 @@
+﻿using Lab06.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab06.Controllers
+{
+    public class EventController : Controller
+    {
+        private static List<EventModel> eventModel = new List<EventModel>()
+        {
+            new EventModel()
+            {
+                ID = 1,
+                name = "Аудиториски вежби по предметот Интернет технологии",
+                location = "Амфитеатар ФИНКИ"
+            },
+            new EventModel()
+            {
+                ID = 2,
+                name = "Предавање по предметот Интернет технологии",
+                location = "117-ТМФ"
+            },
+            new EventModel()
+            {
+                ID = 3,
+                name = "Лабораториски вежби по предметот Интернет технологии",
+                location = "лаб.13-ТМФ"
+            }
+        };
+        // GET: Event
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EventView()
+        {
+            return View(eventModel);
+        }
+
+        public ActionResult AddNewEvent()
+        {
+            EventModel model = new EventModel();
+
+            return View(model);
+        }
+
+        private static int nextID = eventModel.Max(e => e.ID) + 1;
+
+        [HttpPost]
+        public ActionResult AddNewEvent(EventModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //model.ID = eventModel.Count() + 1;
+                //eventModel.Add(model);
+
+                model.ID = nextID;
+                nextID++;
+                eventModel.Add(model);
+
+                return RedirectToAction("EventView", "Event");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        public ActionResult UpdateEvent(int ID)
+        {
+            EventModel e_model = eventModel.Find(x => x.ID == ID);
+
+            return View(e_model);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEvent(EventModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                EventModel e_model = eventModel.Find(x => x.ID == model.ID);
+                e_model.name = model.name;
+                e_model.location = model.location;
+
+                return RedirectToAction("EventView", "Event");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        public ActionResult DeleteEvent(int ID)
+        {
+            EventModel model = eventModel.Find(x => x.ID == ID);
+            eventModel.Remove(model);
+
+            return RedirectToAction("EventView", "Event");
+        }
+    }
+}
---
+++ IT_LAB_06/Lab06/Lab06/Models/EventModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab06.Models
+{
+    public class EventModel
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Името е задолжително")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "Локацијата е задолжителна")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Локацијата мора да биде помеѓу 5 и 30 карактери")]
+        public string location { get; set; }
+    }
+}
